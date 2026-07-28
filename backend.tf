64691cb0bb18e3a78e25e7657b8ba8051662badb
mud1221 <mohdmudasir1947@gmail.com>
2026-07-28 10:41:31 +0530

    Remove duplicate Terraform files from repository root

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mudasir-terraform-state"
-    key            = "jenkins/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
